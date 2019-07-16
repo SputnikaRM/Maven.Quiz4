cade70952adc2559d521f76ac073ddb8173c7906
sputnika <sputnika@zipcoders-MacBook-Pro-5.local>
2019-07-16 09:52:56 -0400

    update

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,23 +1,36 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        return string.charAt(string.length()/2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        int x= str.length()/2;
+        char c= Character.toUpperCase(getMiddleCharacter(str));
+        return str.substring(0,x)+c+str.substring(x+1,str.length());
+
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        char c= Character.toLowerCase(getMiddleCharacter(str));
+
+        return str=str.substring(0,str.length()/2) + c+ str.substring(str.length()/2+1,str.length());
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        char[]arr = str.toCharArray();
+        Arrays.sort(arr);
+        for(int x=0;x<arr.length-1;x++)
+            if(arr[x] == arr[x+1])
+                return false;
+            return true;
+
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
