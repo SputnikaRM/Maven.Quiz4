974a6dd1bbbc58fe8965393c28771c875c460d4a
sputnika <sputnika@zipcoders-MacBook-Pro-5.local>
2019-07-16 11:22:02 -0400

    map done

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,28 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
+    private Map<String,Integer> map=new HashMap<>();
+
     public WordCounter(String... strings) {
+
+        for(String value : strings)
+        {
+            Integer num = map.get(value);
+            if(num == null)
+                map.put(value,1);
+            else
+                map.put(value,num+1);
+        }
+
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+
+
+
+        return map;
     }
 }
