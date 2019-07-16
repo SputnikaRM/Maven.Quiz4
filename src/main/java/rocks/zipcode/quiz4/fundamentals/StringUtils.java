2a0bbc013051bcc0c87c8f4682abe19622d994da
sputnika <sputnika@zipcoders-MacBook-Pro-5.local>
2019-07-16 10:24:34 -0400

    update

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -34,14 +34,33 @@ public class StringUtils {
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        for(int x=0;x<str.length()-1;x++)
+            if(str.charAt(x)==str.charAt(x+1))
+                return true;
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        StringBuilder sb= new StringBuilder(str);
+        System.out.println(sb);
+        for(int x=0; x<sb.length()-1;x++)
+            if(sb.charAt(x) == sb.charAt(x+1)){
+                sb.deleteCharAt(x+1);
+                sb.deleteCharAt(x);}
+        System.out.println(sb);
+            return sb.toString();
     }
 
     public static String invertCasing(String str) {
-        return null;
+        char [ ]arr = str.toCharArray();
+
+        for(int x =0;x<arr.length;x++){
+            char c = arr[x];
+            if(Character.isUpperCase(c))
+              arr[x] = Character.toLowerCase(c);
+            else if (Character.isLowerCase(c))
+             arr[x]=   Character.toUpperCase(c);
+        }
+        return new String(arr);
     }
 }
